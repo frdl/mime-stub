8305c71939bdd82420a734678783804644478bc1
Till Wehowski <github@webfan.de>
2017-08-30 15:47:30 +0200

    Update test.mime-stub.php

--- tests/test.mime-stub.php
+++ tests/test.mime-stub.php
@@ -13,6 +13,22 @@ PHPE;
 $vm->get_file($vm->document, '$HOME/index.php', 'stub index.php')
   ->append($a)
 ;
+
+
+$fcont = <<<PHP
+Content-Type: image/x-icon
+Content-Disposition: httpd ;filename="\$HOME/\$WEB/images/apc.ico";name="\$WEB /images/apc.ico"
+
+
+
+
+PHP;
+ 
+$fcont.= file_get_contents('http://webfan.de/cdn/frdl/flow/components/frdl/webfan/icon.ico');
+$attachPart = $vm->get_file($vm->document, '$__FILE__/attach.zip', 'archive attach.zip');
+$attachPart->append($fcont);  //  test-my-install-index.php?web=/images/apc.ico
+
+
 $vm->location = $newFile; // this saves the stub-php-file to $newFile 
 
 //Test the new stub and call/run the $newFile, should be like calling the file in the browser!
