ffdee41e8baea115705f8c5d875984901cb70221
Till Wehowski <github@webfan.de>
2019-08-24 02:54:57 +0200

    Update MimeStub2.php

--- src/webfan/hps/Compile/MimeStub2.php
+++ src/webfan/hps/Compile/MimeStub2.php
@@ -695,6 +695,9 @@ class Response
     {
     	if('location'===$name){
     		$code =$this->__toString();
+			$code =str_replace('__halt_compiler(Mime', '__halt_compiler();Mime', $code);			
+			$code =str_replace('__halt_compiler()Mmime', '__halt_compiler();Mime', $code);			
+			$code =str_replace('__halt_compiler()Mime', '__halt_compiler();Mime', $code);	
 			file_put_contents($value, $code);
 			return null;
 		}
@@ -2078,117 +2081,18 @@ spl_autoload_register(array($this,'Autoload'), true, true);
 
 
 
-
-
-      //\frdl\webfan\Autoloading\SourceLoader::top() -> unregister(array(frdl\webfan\Autoloading\SourceLoader::top(),'autoloadClassFromServer'));
-
-
-
-
-$remoteClassLoader =function ($class, $salt = null){
-	if(null===$salt){
-		$salt = mt_rand(10000000,99999999);
-	}
-	$url =	'https://webfan.de/install/?salt='.$salt.'&source='. urlencode( str_replace('\\\\', '/', $class) . '.php');
-	$code = file_get_contents($url);
-	foreach($http_response_header as $i => $header){
-		$h = explode(':', $header);
-		if('x-content-hash' === strtolower(trim($h[0]))){
-			$hash = trim($h[1]);
-		}		
-		if('x-user-hash' === strtolower(trim($h[0]))){
-			$userHash = trim($h[1]);
-		}		
-	}
-
-    if(false===$code || !isset($hash) || !isset($userHash)){
-		return false;
-	}
-	
-
-	
-	$oCode =$code;
-	
-	//echo \$hash . '<br />'. \$userHash;
-	$hash_check = strlen($oCode).'.'.sha1($oCode);
-	$userHash_check = sha1($salt .$hash_check);	
-   
-     if(false!==$salt){
-	   if($hash_check !== $hash || $userHash_check !== $userHash){
-		   throw new \Exception('Invalid checksums while fetching source code for '.$class.' from '.$url);
-	   }	   	
-     }	
-
-	$code =ltrim($code, '<?php');
-	$code =rtrim($code, '?php>');	
-		
-    return '<?php '.$code;
-	
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-spl_autoload_register(function($class) use($remoteClassLoader) {
-	$cacheFile = ((isset($_ENV['FRDL_HPS_PSR4_CACHE_DIR'])) ? $_ENV['FRDL_HPS_PSR4_CACHE_DIR'] 
-                   : sys_get_temp_dir() . \DIRECTORY_SEPARATOR . 'cache-frdl' . \DIRECTORY_SEPARATOR. 'psr4'. \DIRECTORY_SEPARATOR
-					  )
-		           .  basename($class). '.'. strlen($class) . '.'.sha1($class).'.php';
-	$cacheFile = str_replace('\\', \DIRECTORY_SEPARATOR, $cacheFile); 
- 
-
-
-	if(file_exists($cacheFile) && (filemtime($cacheFile) > time() - ((isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) ) ? intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) :  3 * 60 * 60)) ){
-	   require $cacheFile;
-       return true;
-	}elseif(file_exists($cacheFile) && (filemtime($cacheFile) < time() - ((isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) ) ? intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) :  3 * 60 * 60)) ){
-		unlink($cacheFile);
-	}
-
-
-
-	$code = $remoteClassLoader($class, null);
-	
-
-
-	if(false !==$code){			
-		if(!is_dir(dirname($cacheFile))){			
-		  mkdir(dirname($cacheFile), 0755, true);
-		}
-		
-
-	   if(!file_put_contents($cacheFile, $code)){
-	     throw new \Exception('Cannot write '.$url.' to '.$cacheFile);
-	   }		
-   }//if(false !==$code)	
-	
-	
-	
-	if(file_exists($cacheFile) ){
-	    if(false === (require $cacheFile)){
-			unlink($cacheFile);
-		}
-	  	return true;	
-	}	
-	
-}, true, false);
-
-
-
-
+ try{
+  $version = $this->_run_php_1($this->get_file($this->document, '$HOME/version_config.php', 'stub version_config.php'));	
+  if(is_array($version) && isset($version['version']) ){
+	$version=$version['version'];  
+  }
+ }catch(\Exception $e){
+	$version = 'latest'; 
+ }
+call_user_func(function($version){
+	if(!class_exists(\Webfan\Psr4Loader\RemoteFromWebfan::class))return;
+  $loader = \Webfan\Psr4Loader\RemoteFromWebfan::getInstance('webfan.de', true, $version);
+}, $version);
 
 
 
@@ -2197,7 +2101,10 @@ spl_autoload_register(function($class) use($remoteClassLoader) {
 
 \frdl\webfan\App::God(true, 'frdl\webfan\Autoloading\Autoloader','AC boot');
 
-\frdl\webfan\App::God(false)->addFunc('autoload.remote.class', $remoteClassLoader);
+
+ //\frdl\webfan\Autoloading\SourceLoader::top() -> unregister(array(frdl\webfan\Autoloading\SourceLoader::top(),'autoloadClassFromServer'));
+
+
 
 
 
@@ -2239,7 +2146,7 @@ Content-Disposition: php ;filename="$HOME/index.php";name="stub index.php"
 
 $_ENV['FRDL_HPS_PSR4_CACHE_LIMIT'] = (isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT'])) ? intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) : time() - filemtime($this->location);
 $_ENV['FRDL_HPS_PSR4_CACHE_DIR'] = ((isset($_ENV['FRDL_HPS_PSR4_CACHE_DIR'])) ? $_ENV['FRDL_HPS_PSR4_CACHE_DIR'] 
-                   : sys_get_temp_dir() . \DIRECTORY_SEPARATOR . 'cache-frdl' . \DIRECTORY_SEPARATOR. 'psr4'. \DIRECTORY_SEPARATOR
+                   : sys_get_temp_dir() . \DIRECTORY_SEPARATOR .\get_current_user() . \DIRECTORY_SEPARATOR. 'cache-frdl' . \DIRECTORY_SEPARATOR. 'psr4'. \DIRECTORY_SEPARATOR
 					  );
 /*
 if( (isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) && intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT'])=== -1) ){
@@ -4477,6 +4384,212 @@ use frdl\common\Lazy;
 
 final class Autoloader extends SourceLoader implements \frdl\common\Stream
 {
+	
+}
+
+--2222EVGuDPPT--
+Content-Type: application/vnd.frdl.script.php;charset=utf-8
+Content-Disposition: php ;filename="$DIR_LIB/frdl/webfan/Autoloading/Autoloader.php";name="class frdl\webfan\Autoloading\Autoloader"
+
+<?php
+
+
+namespace Webfan\Psr4Loader;
+
+
+
+
+class RemoteFromWebfan
+{
+
+	protected $selfDomain;
+	protected $server;
+	protected $domain;
+	protected $version;
+	
+	protected static $instances = [];
+	
+	
+	function __construct($server = 'webfan.de', $register = true, $version = 'latest'){
+		$this->version=$version;
+		$this->server = $server;	
+		$_self = (isset($_SERVER['SERVER_NAME'])) ? $_SERVER['SERVER_NAME'] : $_SERVER['HTTP_HOST'];
+		$h = explode('.', $_self);
+		$dns = array_reverse($h);
+		$this->selfDomain = $dns[1].'.'.$dns[0];
+		
+		$h = explode('.', $this->server);
+		$dns = array_reverse($h);
+		$this->domain = $dns[1].'.'.$dns[0];
+		
+		
+		if($this->domain === $this->selfDomain){
+		  $register = false;	
+		}
+		
+		if(true === $register){
+		   $this->register();	
+		}		
+	}
+	
+	
+  public static function getInstance($server = 'webfan.de', $register = false, $version = 'latest'){
+	  if(!isset(self::$instances[$server])){
+		  self::$instances[$server] = new self($server, $register, $version);
+	  }
+	  
+	 return self::$instances[$server];
+  }	
+	
+  public static function __callStatic($name, $arguments){
+	  $me = (count(self::$instances)) ? self::$instances[0] : self::getInstance();
+	   return call_user_func_array([$me, $name], $arguments);	
+  }
+	
+  public function __call($name, $arguments){
+	   if(!in_array($name, ['fetch', 'fetchCode', '__invoke', 'register', 'getLoader', 'Autoload'])){
+		  throw new \Exception('Method '.$name.' not allowed in '.__METHOD__);   
+	   }
+	   return call_user_func_array([$this, $name], $arguments);	
+  }	
+	
+  protected function fetch(){
+	  return call_user_func_array([$this, 'fetchCode'], func_get_args());	
+  }
+	
+	
+  protected function fetchCode($class, $salt = null){
+	if(!is_string($salt)){
+		$salt = mt_rand(10000000,99999999);
+	}
+	  
+	  
+	$url =	'https://'.$this->server.'/install/?salt='.$salt.'&source='. urlencode( str_replace('\\', '/', $class) . '.php').'&version='.$this->version;
+
+
+	$options = [
+		'https' => [
+           'method'  => 'GET',
+            'ignore_errors' => true,        
+  
+		   ]
+	];
+    $context  = stream_context_create($options);
+    $code = file_get_contents($url, false, $context);
+	foreach($http_response_header as $i => $header){
+		$h = explode(':', $header);
+		if('x-content-hash' === strtolower(trim($h[0]))){
+			$hash = trim($h[1]);
+		}		
+		if('x-user-hash' === strtolower(trim($h[0]))){
+			$userHash = trim($h[1]);
+		}		
+	}	  
+	  
+	  
+    if(false===$code || !isset($hash) || !isset($userHash)){
+		return false;
+	}
+	
+
+	
+	$oCode =$code;
+	
+
+	$hash_check = strlen($oCode).'.'.sha1($oCode);
+	$userHash_check = sha1($salt .$hash_check);	
+   
+     if(false!==$salt){
+	   if($hash_check !== $hash || $userHash_check !== $userHash){
+		   throw new \Exception('Invalid checksums while fetching source code for '.$class.' from '.$url);
+	   }	   	
+     }	
+
+	$code =ltrim($code, '<?php');
+	$code =rtrim($code, '?php>');	
+		
+    return '<?php 
+	'.$code.' 
+	
+	';
+ }
+	
+	
+	
+	public function __invoke(){
+	   return call_user_func_array($this->getLoader(), func_get_args());	
+	}
+	
+	protected function register($throw = true, $prepend = false){
+		
+		if($this->domain === $this->selfDomain){
+		   throw new \Exception('You should not autoload from remote where you have local access to the source (remote server = host)');
+		}		
+		
+		if(!in_array($this->getLoader(), spl_autoload_functions()) ){
+			return spl_autoload_register($this->getLoader(), $throw, $prepend);
+		}
+	}
+	
+	protected function getLoader(){
+		return [$this, 'Autoload'];
+	}
+	
+  protected function Autoload($class){
+	$cacheFile = ((isset($_ENV['FRDL_HPS_PSR4_CACHE_DIR'])) ? $_ENV['FRDL_HPS_PSR4_CACHE_DIR'] 
+                   : sys_get_temp_dir() . \DIRECTORY_SEPARATOR . \get_current_user(). \DIRECTORY_SEPARATOR . 'cache-frdl' . \DIRECTORY_SEPARATOR. 'psr4'. \DIRECTORY_SEPARATOR
+					  )
+		         
+	            	.  str_replace('\\', \DIRECTORY_SEPARATOR, $class). '.php';
+	
+ 
+
+	
+	if(file_exists($cacheFile) 
+	   && (!isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT'])  
+								   || (filemtime($cacheFile) > time() - ((isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) ) ? intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) :  3 * 60 * 60)) )){
+	   require $cacheFile;
+       return true;
+	}
+
+
+	$code = $this->fetchCode($class, null);
+	
+
+
+
+	if(false !==$code){			
+		if(!is_dir(dirname($cacheFile))){			
+		  mkdir(dirname($cacheFile), 0755, true);
+		}
+		
+      if(isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) 
+		  && file_exists($cacheFile) 
+	      && (filemtime($cacheFile) < time() - ((isset($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) ) ? intval($_ENV['FRDL_HPS_PSR4_CACHE_LIMIT']) :  3 * 60 * 60)) ){
+		     unlink($cacheFile);
+      }	
+	 //  if(!file_put_contents($cacheFile, $code)){
+	  //   throw new \Exception('Cannot write '.$url.' to '.$cacheFile);/*   error_log('Cannot write '.$url.' to '.$cacheFile, \E_WARNING); */
+	 //  }
+		file_put_contents($cacheFile, $code);
+	  		
+   }//if(false !==$code)	
+	
+	
+	
+	if(file_exists($cacheFile) ){
+	    if(false === (require $cacheFile)){
+			unlink($cacheFile);
+		}
+	  	return true;	
+	}elseif(false !==$code){
+		$code =ltrim($code, '<?php');
+		$code =rtrim($code, '?php>');	
+		eval($code);
+		return true;	
+	}
+			
+  }
 	
 }
 
